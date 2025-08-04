a206a8b6ac27b2dd6a2f89d0671023ab6a420af5
duykien1310 <kien.nguyenduy.vnu@gmail.com>
2025-08-04 23:05:28 +0700

    feat: implement threads pool

---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strconv"
+	"sync"
+)
+
+const (
+	BUFFER_SIZE       = 2048
+	THREAD_QUEUE_SIZE = 100
+	THREAD_POOL_SIZE  = 5
+)
+
+type Message struct {
+	WorkerId int
+	From     string
+	Data     []byte
+}
+
+type Server struct {
+	Host       string
+	Port       int
+	Ln         net.Listener
+	Msgch      chan Message
+	quitch     chan struct{}
+	ThreadPool *ThreadPool
+}
+
+type ThreadPool struct {
+	Wg          sync.WaitGroup
+	ThreadQueue chan Thread
+}
+
+type Thread struct {
+	Conn net.Conn
+}
+
+func NewServer(host string, port int) *Server {
+	return &Server{
+		Host:       host,
+		Port:       port,
+		Msgch:      make(chan Message),
+		quitch:     make(chan struct{}),
+		ThreadPool: NewThreadPool(THREAD_POOL_SIZE),
+	}
+}
+
+func NewThreadPool(poolSize int) *ThreadPool {
+	pool := &ThreadPool{
+		ThreadQueue: make(chan Thread, THREAD_QUEUE_SIZE),
+	}
+	pool.Wg.Add(poolSize)
+
+	return pool
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%v", s.Port))
+	if err != nil {
+		fmt.Println("Start Listen Err: ", err.Error())
+		return err
+	}
+	defer ln.Close()
+	s.Ln = ln
+
+	fmt.Println("Server listening on", s.Ln.Addr())
+
+	// Start worker
+	for i := 0; i < THREAD_POOL_SIZE; i++ {
+		go s.Process(i)
+	}
+
+	go s.AcceptListen()
+
+	<-s.quitch
+	close(s.Msgch)
+	close(s.ThreadPool.ThreadQueue)
+
+	return nil
+}
+
+func (s *Server) Process(workerId int) {
+	defer s.ThreadPool.Wg.Done()
+	for thread := range s.ThreadPool.ThreadQueue {
+		s.Read(thread.Conn, workerId)
+	}
+}
+
+func (s *Server) AcceptListen() {
+	for {
+		conn, err := s.Ln.Accept()
+		if err != nil {
+			fmt.Println("Accept Listen Err: ", err.Error())
+			continue
+		}
+
+		fmt.Println("New connection to the server: ", conn.RemoteAddr().String())
+
+		s.ThreadPool.ThreadQueue <- Thread{
+			Conn: conn,
+		}
+	}
+}
+
+func (s *Server) Read(conn net.Conn, workerId int) {
+	defer conn.Close()
+	buf := make([]byte, BUFFER_SIZE)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Client disconnected:", conn.RemoteAddr())
+				return
+			}
+			fmt.Println("Read error:", err)
+			continue
+		}
+
+		msg := buf[:n]
+		s.Msgch <- Message{
+			WorkerId: workerId,
+			From:     conn.RemoteAddr().String(),
+			Data:     msg,
+		}
+
+		conn.Write([]byte("Thank you for your message from workerId " + strconv.Itoa(workerId) + " !\n"))
+	}
+}
+
+func main() {
+	server := NewServer("localhost", 3000)
+
+	go func() {
+		for msg := range server.Msgch {
+			fmt.Println("Addr: " + msg.From + ", WorkerId: " + strconv.Itoa(msg.WorkerId) + " : " + string(msg.Data))
+		}
+	}()
+
+	err := server.Start()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
