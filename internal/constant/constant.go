5133c53f5f13bce6c05cfcf6e7d1695ff54ea977
duykien1310 <kien.nguyenduy.vnu@gmail.com>
2025-08-22 11:56:39 +0700

    fix: remove redundant constant

--- internal/constant/constant.go
+++ internal/constant/constant.go
@@ -1,16 +1,5 @@
 package constant
 
-import "time"
-
-var RespNil = []byte("$-1\r\n")
-var RespOk = []byte("+OK\r\n")
-var TtlKeyNotExist = []byte(":-2\r\n")
-var TtlKeyExistNoExpire = []byte(":-1\r\n")
-
-const ActiveExpireFrequency = 100 * time.Millisecond
-const ActiveExpireSampleSize = 20
-const ActiveExpireThreshold = 0.1
-const MaxConnection = 1000
-
+const MaxConnection = 20000
 const OpRead = 0
 const OpWrite = 1
